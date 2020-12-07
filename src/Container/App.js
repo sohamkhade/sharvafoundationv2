5c32ed0c65e3fd20c2dc2148a0ef9d51cf9a6836
sohamkhade <55399257+sohamkhade@users.noreply.github.com>
2020-12-07 11:06:49 +0530

    Update App.js

--- src/Container/App.js
+++ src/Container/App.js
@@ -3,11 +3,15 @@ import Navigation from '../Component/Navigation/Navigation.js';
 import Logo from '../Image/Logo.png';
 import './App.css';
 
+import Counter from '../Component/CounterUp/counter.jsx';
+import '../Component/CounterUp/counterUp.css'
+
 class App extends Component {
   render() {
     return(
       <div className="App">
         <Navigation src = {Logo}/>
+        <Counter />
       </div>
     );
   }
